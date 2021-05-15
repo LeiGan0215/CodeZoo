bf336537ed64ce7f4315bd5da0b897fd3d46bfeb
GL0215 <2805018168@qq.com>
2021-05-15 20:42:54 +0800

    add GIoU

--- detection/cal_IoU.py
+++ detection/cal_IoU.py
@@ -32,14 +32,27 @@ def cal_GIoU(boxA, boxB):
     inter = w * h
     # IoU
     IoU = inter / (area_A + area_B - inter)
-    return IoU
+    # 并集面积
+    A_B = area_A + area_B - inter
+    # 求最小闭包面积
+    x1 = np.minimum(boxA[:, 0], boxB[:, 0])
+    y1 = np.minimum(boxA[:, 1], boxB[:, 1])
+    x2 = np.maximum(boxA[:, 2], boxB[:, 2])
+    y2 = np.maximum(boxA[:, 3], boxB[:, 3])
+    w1 = np.maximum(0.0, x2 - x1 + 1)
+    h1 = np.maximum(0.0, y2 - y1 + 1)
+    area_C = w1 * h1
+    # 求GIoU
+    GIoU = IoU - (area_C - A_B) / area_C
+    return GIoU
 
 
 if __name__ == '__main__':
     boxA = np.array([[5, 5, 10, 10], [5, 5, 10, 10], [5, 5, 10, 10], [5, 5, 10, 10]])
     boxB = np.array([[5, 5, 10, 10], [7, 7, 15, 15], [1, 1, 8, 8], [6, 6, 9, 9]])
+    # IoU范围：[0,1]
     IoU = cal_IoU(boxA, boxB)
     print("IoU={}".format(IoU))
-
+    # GIoU范围：[-1,1]
     GIoU = cal_GIoU(boxA, boxB)
     print("GIoU={}".format(GIoU))
